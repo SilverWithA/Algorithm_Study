378d93c34df36ab09ddb7e396a2d88fbb0c304d9
silver a <92441328+SilverWithA@users.noreply.github.com>
2023-07-03 20:49:16 +0900

    Update 체육복.py

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,7 +1,7 @@
 import copy
 def solution(n, lost, reserve):
     
-# lost가 빌릴 수 있는지 결정ㅈ의 우선순위 -> -1인 번 학생에게 먼저 빌리기
+    # lost와 reserve 중복 학생 번호 제거(교집합 제거)
     lost_n = list(set(lost) - set(reserve))
     reserve_n = list(set(reserve) - set(lost))
     lost_num = 0
