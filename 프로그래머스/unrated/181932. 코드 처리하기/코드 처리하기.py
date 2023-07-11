678d614f954a7a8bdc637760b90c4ce4aa1ea971
silver a <92441328+SilverWithA@users.noreply.github.com>
2023-07-11 12:26:16 +0900

    Update 코드 처리하기.py

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -24,3 +24,19 @@ def solution(code):
         return ret
     else:
         return "EMPTY"
+
+
+
+#---------------------- < 다른 풀이 > ------------------------------
+# def solution(code):
+#     answer = ''
+
+#     mode = 0
+#     for i in range(len(code)):
+#         if code[i] == '1':
+#             mode ^= 1
+#         else:
+#             if i % 2 == mode:
+#                 answer += code[i]
+
+#     return answer if answer else 'EMPTY'
