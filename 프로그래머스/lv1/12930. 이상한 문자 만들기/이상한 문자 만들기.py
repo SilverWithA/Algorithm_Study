b573ad3c2601b8e583cf90635e54bffdf11327ed
silver a <92441328+SilverWithA@users.noreply.github.com>
2023-09-05 19:05:35 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,22 @@
+def solution(s):
+    s = s.lower()
+    words = s.split(' ')
+    # print(words)
+    
+    answer = []
+    
+    for word in words:
+        temp = ''
+        for i in range(len(word)):
+            if i % 2 == 0:
+                temp += word[i].upper()
+            else:
+                temp += word[i].lower()
+        answer.append(temp)
+
+    result = ''
+    for i in range(len(answer)):
+        result += answer[i] + " "
+                
+    return result[:-1]
+                
