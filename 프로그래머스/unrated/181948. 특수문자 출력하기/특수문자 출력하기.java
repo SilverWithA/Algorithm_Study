d885945d9c31f3c2c277ad2343639ea45708fd3d
silver a <92441328+SilverWithA@users.noreply.github.com>
2023-10-12 15:29:23 +0900

    [level unrated] Title: 특수문자 출력하기, Time: 169.96 ms, Memory: 59.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        // 백슬래시 사용: `와 " 출력하려면 앞에 백슬래시 하나를 붙여준다
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+        
+    }
+}
