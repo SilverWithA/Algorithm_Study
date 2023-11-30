9295ccebade8bbf8fde47fd0149971bb79bd9d03
silver a <92441328+SilverWithA@users.noreply.github.com>
2023-11-30 21:23:15 +0900

    Update 시소 짝꿍.java

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,32 +1,4 @@
 import java.util.*;
-// class Solution {
-//     public int solution(int[] weights) {
-//         int answer = 0;
-        
-//         Arrays.sort(weights);
-        
-//         Map<Double, Integer> result = new HashMap<>();
-        
-//         // 1:1 이거나 2:3이거나 2:4, 3:4 중 하나
-//         for(int weight : weights){
-//             double a = weight * 1.0;
-//             double b = (weight * 3.0)/4.0;
-//             double c = (weight * 2.0)/3.0;
-//             double d = (weight * 1.0)/2.0;
-            
-//             if(result.containsKey(a)) answer += result.get(a);
-//             if(result.containsKey(b)) answer += result.get(b);
-//             if(result.containsKey(c)) answer += result.get(c);
-//             if(result.containsKey(d)) answer += result.get(d);
-            
-//             result.put((weight * 1.0), result.getOrDefault((weight * 1.0),0) + 1);
-                       
-            
-//         }
-//         return answer;
-//     }
-// }
-
 
 class Solution {
     public long solution(int[] weights) {
