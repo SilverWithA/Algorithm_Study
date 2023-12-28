1c5be63f8dd78fa054859614cd2f96ac1f2d85b1
silver a <92441328+SilverWithA@users.noreply.github.com>
2023-12-28 11:59:43 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,18 +1,13 @@
-n = int(input())
-arr = []
-for cnt in range(n):
-    arr.append(input())
-
 answer = 0
+for _ in range(int(input())):
+    tmp = ''
+    for char in input():
+        if char not in tmp:
+            tmp += char
 
-for word in arr:
-    tmep = []
-    tmep.append(word[0])
-    for j in range(len(word)-1):
-        if word[j] != word[j+1]:
-            tmep.append(word[j+1])
-
-    if len(tmep) == len(list(set(tmep))):
+        elif char != tmp[-1:]:
+            break
+    else:
         answer += 1
 
 print(answer)
