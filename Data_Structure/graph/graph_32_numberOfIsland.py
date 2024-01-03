8b9bbb8f90d71e2f2f0eaaeac25b59c987a19536
silver a <92441328+SilverWithA@users.noreply.github.com>
2024-01-03 10:48:55 +0900

    Add files via upload

---
+++ Data_Structure/graph/graph_32_numberOfIsland.py
@@ -0,0 +1,27 @@
+def numIsland(grid):
+    def dfs(i, j):
+        if i < 0 or j > len(grid) or j < 0 or i > len(grid) or grid[i][j] != '1':
+            return
+        grid[i][j] = 0
+
+        dfs(i + 1, j)
+        dfs(i - 1, j)
+        dfs(i, j + 1)
+        dfs(i, j - 1)
+
+    cnt = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '1':
+                dfs(i, j)
+                cnt += 1
+    return cnt
+
+grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+
+print(numIsland(grid))
---
+++ Data_Structure/graph/graph_33_letterCombinationPhone.py
@@ -0,0 +1,24 @@
+def phoneNumberCombination(digits):
+    def dfs(index, path):
+        # 끝까지 탐색하면 백트래킹
+        if len(path) == len(digits):
+            result.append(path)
+            return
+
+        # 입력값 자릿수 단위 반복
+        for i in range(index, len(digits)):
+            for j in dic[digits[i]]:
+                dfs(i + 1, path + j)
+
+    if not digits:
+        return []
+
+    dic = {'2': "abc", '3': "def", '4': "ghi"}
+    result = []
+    dfs(0, "")
+
+    return result
+
+
+phoneNumberCombination("34")
+
