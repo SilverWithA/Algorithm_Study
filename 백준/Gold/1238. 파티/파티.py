fecd939a4a5815e9f1654202080dcbeb0a296caf
silver a <92441328+SilverWithA@users.noreply.github.com>
2024-01-29 08:45:21 +0900

    [Gold III] Title: 파티, Time: 1760 ms, Memory: 34212 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -3,37 +3,33 @@ import sys
 
 input = sys.stdin.readline
 INF = int(1e9)
-# 입력 받기
-std, edge, destin = map(int, input().split())
+
+std, edge, x = map(int, input().split())
 graph = [[] for _ in range(std + 1)]
 for _ in range(edge):
     start, end, weight = map(int, input().split())
-    graph[start].append((end, weight))
-# print(graph)
+    graph[start].append((end,weight))
 
-
-# 함수로 구성하기
 def dijkstra(start):
-    q = []
-    distance = [INF] * (std + 1)
-    heapq.heappush(q, (0, start))
+    Q = []
+    heapq.heappush(Q, (0, start))
+    distance = [INF] * (std+1)
     distance[start] = 0
 
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
+    while Q:
+        weight, node = heapq.heappop(Q)
+        if distance[node] < weight:
             continue
-
-        for node_index, node_cost in graph[now]:
-            cost = dist + node_cost
-            if distance[node_index] > cost:
-                distance[node_index] = cost
-                heapq.heappush(q, (cost, node_index))
+        for next_node, next_weight in graph[node]:
+            alt = weight + next_weight
+            if alt < distance[next_node]:
+                distance[next_node] = alt
+                heapq.heappush(Q, (alt, next_node))
     return distance
 
-result = 0
-for i in range(1 ,std+1):
+res = 0
+for i in range(1, std +1):
     go = dijkstra(i)
-    back = dijkstra(destin)
-    result = max(result, go[destin]+ back[i])
-print(result)
+    come = dijkstra(x)
+    res = max(res, go[x] + come[i])
+print(res)
