57baa1e59a0df1a5f1d502cd75848bb00f818ae6
silver a <92441328+SilverWithA@users.noreply.github.com>
2024-01-28 16:08:39 +0900

    [Gold III] Title: 파티, Time: 1772 ms, Memory: 34212 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -3,42 +3,37 @@ import sys
 
 input = sys.stdin.readline
 INF = int(1e9)
+# 입력 받기
+std, edge, destin = map(int, input().split())
+graph = [[] for _ in range(std + 1)]
+for _ in range(edge):
+    start, end, weight = map(int, input().split())
+    graph[start].append((end, weight))
+# print(graph)
 
-v, e, x = map(int, input().split())
-graph = [[] for _ in range(v + 1)]
 
-for _ in range(e):
-    a, b, cost = map(int, input().split())
-    graph[a].append((b, cost))
-
-
+# 함수로 구성하기
 def dijkstra(start):
     q = []
-    distance = [INF] * (v + 1)
-
+    distance = [INF] * (std + 1)
     heapq.heappush(q, (0, start))
     distance[start] = 0
 
     while q:
         dist, now = heapq.heappop(q)
-
         if distance[now] < dist:
             continue
 
         for node_index, node_cost in graph[now]:
             cost = dist + node_cost
-
             if distance[node_index] > cost:
                 distance[node_index] = cost
                 heapq.heappush(q, (cost, node_index))
-
     return distance
 
-
 result = 0
-for i in range(1, v + 1):
+for i in range(1 ,std+1):
     go = dijkstra(i)
-    back = dijkstra(x)
-    result = max(result, go[x] + back[i])
-
+    back = dijkstra(destin)
+    result = max(result, go[destin]+ back[i])
 print(result)
