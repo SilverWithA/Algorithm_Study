f77871c72bb17d81e8611220651d6e11945d87bc
silver a <92441328+SilverWithA@users.noreply.github.com>
2024-07-31 23:01:32 +0900

    [Silver III] Title: 1로 만들기, Time: 588 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+res = [0] * (n + 1)
+
+for i in range(2, n+1):
+    res[i] = res[i-1] + 1
+
+    if i % 2 == 0:
+        res[i] = min(res[i], res[i//2] + 1)
+    if i % 3 == 0:
+        res[i] = min(res[i], res[i//3] + 1)
+print(res[n])
